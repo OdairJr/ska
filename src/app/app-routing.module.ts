d5e334d6be50c86c03dd3eb4bfbe2c792d210440
Odair Junior <odairjunior.oficial@hotmail.com>
2023-09-14 23:19:59 -0300

    FEAT: Adicionando paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule) },
+  { path: 'sign-in', loadChildren: () => import('./modules/sign-in/sign-in.module').then(m => m.SignInModule) },
+  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+  { path: 'details', loadChildren: () => import('./modules/details/details.module').then(m => m.DetailsModule) },
+  { path: 'list', loadChildren: () => import('./modules/list/list.module').then(m => m.ListModule) },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
