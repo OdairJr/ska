f8826d49bca3c3fe6ab460b2d7e9d6b0fb583608
Odair Junior <odairjunior.oficial@hotmail.com>
2023-09-23 16:20:06 -0300

    FEAT: Adicionando Firebase

---
+++ src/app/core/services/firebase.service.ts
@@ -0,0 +1,40 @@
+import { Inject, Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+import { BaseModel } from '../models/base.model';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireBaseService<T extends BaseModel> {
+
+  constructor(private _firestore: AngularFirestore, @Inject(String) private _collectionName: string) { }
+
+  public getAll(): Observable<T[]> {
+    return this._firestore.collection<T>(this._collectionName).snapshotChanges()
+      .pipe(map((response) => {
+        return response.reduce<Array<T>>((previous, current) => {
+          previous.push({
+            id: current.payload.doc.id,
+            ...current.payload.doc.data()
+          });
+          return previous;
+        }, []);
+      }));
+  }
+
+  create(object: T): Observable<string> {
+    const promise = this._firestore.collection<T>(this._collectionName).add(object);
+    return from(promise).pipe(map((response) => response.id));
+  }
+
+  update(object: T) {
+    delete object.id;
+    this._firestore.doc(`${this._collectionName}/${object.id}`).update(object);
+  }
+
+  delete(objectId: string): Observable<void> {
+    return from(this._firestore.doc(`${this._collectionName}/${objectId}`).delete());
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'lasanha-db',
+    appId: '1:2728048523:web:a03cb1242894dcf724618e',
+    storageBucket: 'lasanha-db.appspot.com',
+    apiKey: 'AIzaSyBq6q_HBkbog3IRdT6vcDg1ZI2JZNSs7D8',
+    authDomain: 'lasanha-db.firebaseapp.com',
+    messagingSenderId: '2728048523',
+  },
+};
