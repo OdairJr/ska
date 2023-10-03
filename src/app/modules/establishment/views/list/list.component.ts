4ffe8e787ca32cb6a89a298b61c4d13928d2d3a0
Odair Junior <odairjunior.oficial@hotmail.com>
2023-10-02 22:38:24 -0300

    FEAT: Adicionando filtro

--- src/app/modules/establishment/views/list/list.component.ts
+++ src/app/modules/establishment/views/list/list.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { Observable, map, startWith } from 'rxjs';
+import { Establishment } from 'src/app/core/models/establishment.model';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { EstablishmentService } from 'src/app/core/services/establishment.service';
 
@@ -9,10 +12,13 @@ import { EstablishmentService } from 'src/app/core/services/establishment.servic
   standalone: true,
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
-export class ListComponent {
-  public establishments$ = this.establishmentService.getAll();
+export class ListComponent implements OnInit {
+  public establishments?: Establishment[];
+  public establishmentsFiltered$?: Observable<Establishment[]>;
+
+  public searchControl = new FormControl();
 
   constructor(
     private establishmentService: EstablishmentService,
@@ -20,9 +26,40 @@ export class ListComponent {
     public authService: AuthService
   ) {}
 
+  public ngOnInit(): void {
+    this.establishmentService.getAll().subscribe({
+      next: (resp) => {
+        this.establishments = resp;
+        this.initFilter();
+      },
+    });
+
+
+  }
+
+  private initFilter() {
+    this.establishmentsFiltered$ = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string) {
+    const filterValue = value.toLowerCase();
+
+    return this.establishments?.filter(
+      (estab) =>
+        estab.name.toLowerCase().includes(filterValue) ||
+        estab.description.toLowerCase().includes(filterValue) ||
+        estab.serviceType.toLowerCase().includes(filterValue)
+    );
+  }
+
   public goToDetails(id?: string) {
-    if(!id) return console.error('No id provided');
+    if (!id) return console.error('No id provided');
 
     this.router.navigate(['/establishment/details', id]);
   }
+
+  public applyFilterChange() {}
 }
